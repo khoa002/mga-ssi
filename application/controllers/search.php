af0c4e6b7538172c6767ea8aaa59bc859cefc47b
Khoa Nguyen <khoa002@gmail.com>
2015-04-27 03:26:15 -0700

    no message

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Search extends CI_Controller {
+class search extends CI_Controller {
     public function index() {
         die('test');
         // $this->load->model("searchmodel");
