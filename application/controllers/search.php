1e713704e5f3aca07753296147134221e1105757
unknown <khoa@CNU426CCRC.TagPay.com>
2015-04-27 12:11:07 -0700

    asdf

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Search extends CI_Controller {
     public function __construct(){
         parent::__construct();
+        die('test');
     }
     
     public function index() {
