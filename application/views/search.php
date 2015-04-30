40d2e4a20bd76ea348a8b3dbc53e45bf3359e779
Khoa Nguyen <khoa002@gmail.com>
2015-04-30 12:52:06 -0700

    no message

--- application/views/search.php
+++ application/views/search.php
@@ -74,7 +74,12 @@
 			<p><a type="button" class="btn btn-default btn-sm back-to-degrees-selection">Back</a></p>
 			<form id="tuition" class="form-horizontal">
 				<h3>What is your desired annual tuition rate?</h3>
-				<div id="tuition_slider"></div>
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Tuition</label>
+					<div class="col-sm-10">
+						<div id="tuition_slider"></div>
+					</div>
+				</div>
 			</form>
 			<p style="text-align: right;"><a type="button" class="btn btn-success btn-sm make-tuition-selection">Continue</a></p>
 		</div>
